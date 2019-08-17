2271e1d967a9df47c538848a6c1e670c20e597c3
NishanWijethunga <nishansameerawijethunga@gmail.com>
2019-08-17 22:14:09 +0530

    Smoke added.

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,1 +1,11 @@
+const FullPageJS = require('fullpage.js')
 
+console.log(FullPageJS);
+
+new FullPageJS('#fullpage', {
+	//options here
+	autoScrolling:true,
+	scrollHorizontally: true
+});
+
+require('./smoke')
