ef4b6044ccba6d498ec01608d9642df60194ef2a
MissakaI <m.iddamalgoda@gmail.com>
2019-08-20 21:15:00 +0530

    Inserted content to about section.
    Need work on logo alignment

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,17 +1,17 @@
 import * as $ from 'jquery'
 
-const FullPageJS = require('fullpage.js')
+const FullPageJS = require('fullpage.js');
 
 new FullPageJS('#fullpage', {
 	//options here
 	autoScrolling: true,
 	scrollHorizontally: true,
 	navigation: true,
-	anchors: ['home', 'about', 'shedule', 'registration', 'sponsors', 'contactus'],
+	anchors: ['home', 'about', 'schedule', 'registration', 'organizers', 'contactus'],
 	menu: '#navigation-items-container',
 	responsiveWidth: '768',
 	onLeave: function(origin:any, destination:any, direction:string){
-		const navigation = $.default('#nvigation')
+		const navigation = $.default('#nvigation');
 		if (direction == 'up') {
 			navigation.animate({
 				top:'0'
@@ -29,12 +29,12 @@ new FullPageJS('#fullpage', {
 	},
 });
 
-require('./smoke')
+require('./smoke');
 
-const mobile_responsive_nav_show_btn: any = $.default("#mobile-responsive-btn")
+const mobile_responsive_nav_show_btn: any = $.default("#mobile-responsive-btn");
 
 const mobileResponsiveNavShowBtnClickAction = () => {
-	const nav_dropdown_container = $.default('#navigation-item-container')
+	const nav_dropdown_container = $.default('#navigation-item-container');
 	nav_dropdown_container.slideToggle(300, () => {
 		if (nav_dropdown_container.is(':hidden')) {
 			mobile_responsive_nav_show_btn[0].lastElementChild.textContent = 'menu'
@@ -42,19 +42,19 @@ const mobileResponsiveNavShowBtnClickAction = () => {
 			mobile_responsive_nav_show_btn[0].lastElementChild.textContent = 'close'
 		}
 	})
-}
+};
 mobile_responsive_nav_show_btn.click((e: any) => {
 	mobileResponsiveNavShowBtnClickAction()
-})
+});
 
 $.default('#navigation-items-container .nav-item button').click(e => {
 	if (mobile_responsive_nav_show_btn[0].lastElementChild.textContent == 'close') {
 		mobileResponsiveNavShowBtnClickAction()
 	}
-})
+});
 
 $.default('#fullpage').click(e => {
 	if (mobile_responsive_nav_show_btn[0].lastElementChild.textContent == 'close') {
 		mobileResponsiveNavShowBtnClickAction()
 	}
-})
+});
